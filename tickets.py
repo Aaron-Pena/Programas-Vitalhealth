6e3dcae52346cf573e9e2363706f920e05dc7571
Aaron Peña <aaronpena104@gmail.com>
2023-12-22 10:56:33 -0700

    Ajuste de tabla y cajones
    Se alineo los cajones de texto a la izquierda y la tabla de actividades a la derecha. Ademas se actualiza en tiempo real.

--- tickets.py
+++ tickets.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import pandas 
+import pandas
 from tkinter import *
 from datetime import datetime
 
@@ -8,149 +8,153 @@ def read_last_id_from_file():
     try:
         with open('output.csv', 'r') as file:
             lines = file.readlines()
-            if len(lines) > 1:  
+            if len(lines) > 1:
                 last_entry = lines[-1].split(', ')
                 last_id = int(last_entry[0])
-                return last_id + 1  
+                return last_id + 1
             else:
-                return 1 
+                return 1
     except FileNotFoundError:
-        return 1  
+        return 1
+
 
 def write_last_id_to_file(last_id):
     with open('output.csv', 'a') as file:
         file.write(f'{last_id}\n')
 
-# Create the main window
+
+# Ventana principal
 ws = tk.Tk()
 ws.title("Tickets Vitalhealth")
-ws.geometry('1080x640+250+30')
+ws.geometry('1280x640+100+30')
 current_id = read_last_id_from_file()
 df = pandas.read_csv('output.csv',
                      index_col='id',
                      header=0,
-                     names=['id','Departamento','Numero de serie','Fecha de Generacion','Problema','Status'])
-print (df)
+                     names=['id', 'Departamento', 'Numero de serie', 'Fecha de Generacion', 'Problema', 'Status'])
+print(df)
 
-tk.Label(ws,text="Sistema de tickets...",font=('Arial',15)).pack(fill=tk.BOTH, expand=False)
+tk.Label(ws, text="Sistema de tickets...", font=('Arial', 15)).pack(fill=tk.BOTH, expand=False)
+
+# Crear frame para mover hacia la izquierda
+left_frame = tk.Frame(ws)
+left_frame.pack(side=tk.LEFT, padx=90, pady=60)
 
 def set_current_date():
-    current_date = datetime.now().strftime("%Y-%m-%d")  
-    date_text.delete(1.0, tk.END)  
-    date_text.insert(tk.END, current_date)  
-    
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    date_text.delete(1.0, tk.END)
+    date_text.insert(tk.END, current_date)
+
 
 # ID
-tk.Label(ws, text="Id", font=('Arial', 10)).pack(fill=tk.BOTH, expand=False)
-Id = tk.Text(ws, height=1, width=25, bg="white")
+tk.Label(left_frame, text="Id", font=('Arial', 10)).pack(fill=tk.BOTH, expand=False)
+Id = tk.Text(left_frame, height=1, width=25, bg="white")
 Id.insert(tk.END, str(current_id).zfill(3))
 Id.config(state='disabled')
 Id.pack()
 
-#dropdown menu de departamentos
-tk.Label(ws,text="\nDepartamentos",font=('Arial',10)).pack(fill=tk.BOTH, expand=False)
-dp_variable = tk.StringVar(ws)
-dp_variable.set("---") 
-dropdown = tk.OptionMenu(ws, dp_variable, "Contaduria", "Eventos", "Marketing","Customer Service")
+# dropdown menu de departamentos
+tk.Label(left_frame, text="\nDepartamentos", font=('Arial', 10)).pack(fill=tk.BOTH, expand=False)
+dp_variable = tk.StringVar(left_frame)
+dp_variable.set("---")
+dropdown = tk.OptionMenu(left_frame, dp_variable, "Contaduria", "Eventos", "Marketing", "Customer Service")
 dropdown.pack()
 
-#TextBox editable de Numero de laptop
-tk.Label(ws,text="Numero de serie",font=('Arial',10)).pack(fill=tk.BOTH, expand=False)
-serial = tk.Text(ws, height = 1,
-                width = 25,
-                bg = "white")
+# TextBox editable de Numero de laptop
+tk.Label(left_frame, text="Numero de serie", font=('Arial', 10)).pack(fill=tk.BOTH, expand=False)
+serial = tk.Text(left_frame, height=1, width=25, bg="white")
 serial.pack()
- 
-#TextBox automatico no editable de fecha actual
-tk.Label(ws, text="Fecha", font=('Arial', 10)).pack(fill=tk.BOTH, expand=False)
-date_text = tk.Text(ws, height=1, width=25, bg="white")
+
+# TextBox automatico no editable de fecha actual
+tk.Label(left_frame, text="Fecha", font=('Arial', 10)).pack(fill=tk.BOTH, expand=False)
+date_text = tk.Text(left_frame, height=1, width=25, bg="white")
 date_text.config(state='normal')
 set_current_date()
 date_text.config(state='disabled')
 date_text.pack()
 
-#Textbox Editable de problema
-tk.Label(ws,text="Problema",font=('Arial',10)).pack(fill=tk.BOTH, expand=False)
-problem = tk.Text(ws, height = 7,
-                width = 25,
-                bg = "white")
+# Textbox Editable de problema
+tk.Label(left_frame, text="Problema", font=('Arial', 10)).pack(fill=tk.BOTH, expand=False)
+problem = tk.Text(left_frame, height=7, width=25, bg="white")
 problem.pack()
- 
-#Status no editable
-tk.Label(ws,text="Status",font=('Arial',10)).pack(fill=tk.BOTH, expand=False)
-status = tk.Text(ws, height = 1,
-                width = 25,
-                bg = "white")
+
+# Status no editable
+tk.Label(left_frame, text="Status", font=('Arial', 10)).pack(fill=tk.BOTH, expand=False)
+status = tk.Text(left_frame, height=1, width=25, bg="white")
 status.insert(tk.END, "Incomplete")
 status.config(state='disabled')
 status.pack()
 
-#boton de agregar
+# boton de agregar
 def Agregar_Datos():
-    global current_id 
-    input_data = str(current_id).zfill(3),dp_variable.get(),serial.get("1.0", "end-1c"), date_text.get("1.0", "end-1c"),problem.get("1.0", "end-1c"),status.get("1.0", "end-1c")
+    global current_id
+    input_data = str(current_id).zfill(3), dp_variable.get(), serial.get("1.0", "end-1c"), date_text.get("1.0", "end-1c"), problem.get("1.0", "end-1c"), status.get("1.0", "end-1c")
     print(input_data)
-    
-    # Increment the ID for the next entry
+
+    # Incrementar ID
     current_id += 1
-    
-    # Define the header row
+
+    # Definir header
     header_row = "id, Departamento, Numero de Serie, Fecha de Generacion, Problema, Status"
 
-    # Write the header row if the file is empty
+    # Escribir el header si el archivo esta vacio
     try:
         with open('output.csv', 'r') as file:
             content = file.read()
             if not content.strip():
                 file.write(header_row + '\n')
     except FileNotFoundError:
-        # If the file doesn't exist, create it with the header row
+        # Si no existe el archivo, crear con header
         with open('output.csv', 'w') as file:
             file.write(header_row + '\n')
 
-    # Append the data to the file
+    # Agregar datos al archivo
     with open('output.csv', 'a') as file:
         file.write(', '.join(map(str, input_data)) + '\n')
-       
-    #Actualizar Id
-    Id.config(state='normal')  
+
+    # Actualizar Id
+    Id.config(state='normal')
     Id.delete(1.0, tk.END)
     Id.insert(tk.END, str(current_id).zfill(3))
-    Id.config(state='disabled') 
-    
-    # Actualizar Actividades 
-    df.config(state='normal')  
-    df.delete(1.0, tk.END)
-    df.insert(tk.END, str(df))
-    df.config(state='disabled')  
+    Id.config(state='disabled')
 
-    # Clear the input fields
+    # Actualizar Actividades
+    Act.config(state='normal')
+    df = pandas.read_csv('output.csv', index_col='id',
+                     header=0, names=['id', 'Departamento', 'Numero de serie', 'Fecha de Generacion', 'Problema', 'Status'])
+    Act.delete(1.0, tk.END)
+    Act.insert(tk.END, df.to_string(index=True, col_space=1, justify='right'))
+
+    Act.config(state='disabled')
+
+    # Limpiar campos
     dp_variable.set("---")
     serial.delete(1.0, tk.END)
     set_current_date()
     problem.delete(1.0, tk.END)
     status.delete(1.0, tk.END)
 
-Display = tk.Button(ws, height = 2,
-                 width = 20, 
-                 text ="Agregar",
-                 command = lambda:[Agregar_Datos()])
-
+Display = tk.Button(left_frame, height=2,
+                    width=20,
+                    text="Agregar",
+                    command=lambda: [Agregar_Datos()])
 Display.pack()
 
-#boton de finalizar
-tk.Label(ws,text="\nArea de finalizacion",font=('Arial',10)).pack(fill=tk.BOTH, expand=False)
+# Area de finalizacion
+tk.Label(left_frame, text="\nArea de finalizacion", font=('Arial', 10)).pack(fill=tk.BOTH, expand=False)
 
 # Tabla de actividades
-tk.Label(ws, text="Actividades", font=('Arial', 10)).pack(fill=tk.BOTH, expand=False)
 Act = tk.Text(ws, height=70, width=100, bg="white")
-Act.insert(tk.END, str(df))
+Act.insert(tk.END, df.to_string(index=True))  
 Act.config(state='disabled')
-Act.pack()
+Act.pack(side=tk.RIGHT, padx=10, pady=10)
 
-# Initialize the current ID from the last used ID in the CSV file
+
+ws.columnconfigure(2, weight=2)  
+ws.rowconfigure(1, weight=1)
+
+#Inicializar el archivo con el ultimo ID
 current_id = read_last_id_from_file()
 
-# Start the Tkinter event loop
-
+# Empezar el ciclo de tkinter
 ws.mainloop()
