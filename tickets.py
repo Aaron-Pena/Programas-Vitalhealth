88f6298f37f6b754f9a5598b1924093341813fdd
Aaron Peña <aaronpena104@gmail.com>
2023-12-27 17:49:14 -0700

    Ajuste de tamaño y vista
    Se modifico la vista del programa a una mas usable

--- tickets.py
+++ tickets.py
@@ -22,6 +22,10 @@ def write_last_id_to_file(last_id):
     with open('output.csv', 'a') as file:
         file.write(f'{last_id}\n')
 
+def set_current_date():
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    date_text.delete(1.0, tk.END)
+    date_text.insert(tk.END, current_date)
 
 # Ventana principal
 ws = tk.Tk()
@@ -38,17 +42,12 @@ tk.Label(ws, text="Sistema de tickets...", font=('Arial', 15)).pack(fill=tk.BOTH
 
 # Crear frame para mover hacia la izquierda
 left_frame = tk.Frame(ws)
-left_frame.pack(side=tk.LEFT, padx=90, pady=60)
-
-def set_current_date():
-    current_date = datetime.now().strftime("%Y-%m-%d")
-    date_text.delete(1.0, tk.END)
-    date_text.insert(tk.END, current_date)
+left_frame.pack(side=tk.LEFT, padx=10, pady=60)
 
 
 # ID
 tk.Label(left_frame, text="Id", font=('Arial', 10)).pack(fill=tk.BOTH, expand=False)
-Id = tk.Text(left_frame, height=1, width=25, bg="white")
+Id = tk.Text(left_frame, height=2, width=25, bg="white")
 Id.insert(tk.END, str(current_id))
 Id.config(state='disabled')
 Id.pack()
@@ -62,7 +61,7 @@ dropdown.pack()
 
 # TextBox editable de Numero de laptop
 tk.Label(left_frame, text="Numero de serie", font=('Arial', 10)).pack(fill=tk.BOTH, expand=False)
-serial = tk.Text(left_frame, height=1, width=25, bg="white")
+serial = tk.Text(left_frame, height=2, width=25, bg="white")
 serial.pack()
 
 # TextBox automatico no editable de fecha actual
@@ -179,22 +178,26 @@ Display = tk.Button(left_frame, height=2,
                     command=lambda: [Agregar_Datos()])
 Display.pack()
 
+# Crear frame para la tercera columna
+right_frame = tk.Frame(ws)
+right_frame.pack(side=tk.RIGHT, padx=10, pady=60)
+
 # Area de finalizacion
-tk.Label(left_frame, text="\nArea de finalizacion", font=('Arial', 10)).pack(fill=tk.BOTH, expand=False)
+tk.Label(right_frame, text="\nTabla de actividades", font=('Arial', 10), height=3, width=3).pack(fill=tk.BOTH, expand=False)
 
 # Tabla de actividades
-Act = tk.Text(ws, height=70, width=100, bg="white")
-Act.insert(tk.END, df.to_string(index=True))  
+Act = tk.Text(right_frame, height=70, width=100, bg="white")
+Act.insert(tk.END, df.to_string(index=True))
 Act.config(state='disabled')
-Act.pack(side=tk.RIGHT, padx=10, pady=10)
+Act.pack(side=tk.LEFT, padx=10, pady=10)
 
 # Sección para cambiar el estado a "Completado"
-tk.Label(left_frame, text="\nCambiar Estado a Completado", font=('Arial', 10)).pack(fill=tk.BOTH, expand=False)
+tk.Label(right_frame, text="\nCambiar Estado a Completado", font=('Arial', 10)).pack(fill=tk.BOTH, expand=False)
 
 # Entrada de ID
-id_to_complete_label = tk.Label(left_frame, text="ID a Completar", font=('Arial', 10))
+id_to_complete_label = tk.Label(right_frame, text="ID a Completar", font=('Arial', 10), height=2)
 id_to_complete_label.pack(fill=tk.BOTH, expand=False)
-id_to_complete_entry = tk.Entry(left_frame, width=25)
+id_to_complete_entry = tk.Entry(right_frame, width=25)
 id_to_complete_entry.pack()
 
 # Botón para cambiar estado
@@ -210,13 +213,14 @@ def cambiar_estado():
         Act.delete(1.0, tk.END)
         Act.insert(tk.END, df.to_string(index=True, col_space=0, justify='right'))
         Act.config(state='disabled')
-        id_to_complete_label.config(text="Estado cambiado a 'Completed'", fg="green")
+        id_to_complete_label.config(text="Estado cambiado a 'Completed'", fg="green", height=2)
         print(df)
     except Exception as e:
-        id_to_complete_label.config(text=f"Favor de insertar ID valido", fg="red")
+        id_to_complete_label.config(text=f"Favor de insertar ID valido", fg="red", height=2)
 
-cambiar_estado_button = tk.Button(left_frame, height=2, width=20, text="Cambiar Estado", command=cambiar_estado)
+cambiar_estado_button = tk.Button(right_frame, height=2, width=20, text="Cambiar Estado", command=cambiar_estado)
 cambiar_estado_button.pack()
+
 
 
 ws.columnconfigure(2, weight=2)  
